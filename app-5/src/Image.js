09212dc9a462a4b279860d39ccbca3e243c8f94e
izzy villarini <villariniizzy@gmail.com>
2020-01-29 17:42:32 -0700

    finished

---
+++ app-5/src/Image.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+export default class Image extends Component {
+  render() {
+    return (
+      <div>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/e/ee/Grumpy_Cat_by_Gage_Skidmore.jpg' />
+        <div>409</div>
+        <div>Conflict</div>
+      </div>
+    )
+  }
+}
